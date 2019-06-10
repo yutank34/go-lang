aa7467374fd2538177cc1b81f205273e50451d8c
yutank <yuta34@gmail.com>
2019-06-11 02:33:42 +0900

    feat: from ex01 to ex03

---
+++ ch01/ex02/ex02.go
@@ -0,0 +1,18 @@
+// コマンドライン引数のインデックスと値を1行ずつ表示する
+package ex02
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	Print(os.Stdout, os.Args)
+}
+
+func Print(w io.Writer, args []string) {
+	for i, arg := range args[1:] {
+		fmt.Fprintf(w, "index: %d, arg: %s\n", i, arg)
+	}
+}
---
+++ ch01/ex02/ex02_test.go
@@ -0,0 +1,31 @@
+package ex02
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestEcho(t *testing.T) {
+	var tests = []struct {
+		input []string
+		want  string
+	}{
+		{[]string{"command", ""}, "index: 0, arg: "},
+		{[]string{"command", "h"}, "index: 0, arg: h"},
+		{[]string{"command", "hoge"}, "index: 0, arg: hoge"},
+		{[]string{"command", "hoge", "fuga"}, "index: 0, arg: hoge\nindex: 1, arg: fuga"},
+	}
+
+	for _, test := range tests {
+		buf := new(bytes.Buffer)
+		Print(buf, test.input)
+		output := strings.Trim(string(buf.Bytes()), "\n\t")
+
+		fmt.Println(output)
+		if output != test.want {
+			t.Error("unexpected value:\nexpected:" + test.want + "\nresult:" + output)
+		}
+	}
+}
