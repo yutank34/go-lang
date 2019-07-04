2e9c543ee19b211b016d590dcb17741ebc8e0c87
yutank <yuta34@gmail.com>
2019-07-05 01:58:20 +0900

    第一回課題完了

---
+++ ch02/ex03/popcount.go
@@ -0,0 +1,42 @@
+package popcount
+
+import "fmt"
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCountV1(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func PopCountV2(x uint64) int {
+	var result byte
+	for i := 0; i < 8; i++ {
+		result += pc[byte(x>>uint64(i*8))]
+	}
+	return int(result)
+}
+
+func F() int {
+	// fmt.Print(pc)
+	var t [256]int
+	for i := 0; i < 256; i++ {
+		t[i] = int(i)
+	}
+	for i := 0; i < 256; i++ {
+		fmt.Println((i & 1))
+	}
+	return int(1)
+}
---
+++ ch02/ex05/popcount_test.go
@@ -0,0 +1,68 @@
+package popcount3
+
+import (
+	"testing"
+)
+
+func TestPopCountV2(t *testing.T) {
+	var tests = []struct {
+		input uint64
+		want  int
+	}{
+		{10, 2},
+		{100, 3},
+		{1000, 6},
+	}
+	for _, test := range tests {
+		output := PopCountV2(test.input)
+		if output != test.want {
+			t.Errorf("unexpected value: expected: %v result: %v", test.want, output)
+		}
+	}
+}
+
+func TestPopCountV4(t *testing.T) {
+	var tests = []struct {
+		input uint64
+		want  int
+	}{
+		{10, 2},
+		{100, 3},
+		{1000, 6},
+	}
+	for _, test := range tests {
+		output := PopCountV4(test.input)
+		if output != test.want {
+			t.Errorf("unexpected value: expected: %v result: %v", test.want, output)
+		}
+	}
+}
+
+func makeInputs() []uint64 {
+	var ints []uint64
+	for i := 10000; i < 10000000; i++ {
+		ints = append(ints, uint64(i))
+	}
+	return ints
+}
+
+func BenchmarkPopCountV2(b *testing.B) {
+	var tests = makeInputs()
+	for _, v := range tests {
+		PopCountV2(v)
+	}
+}
+
+func BenchmarkPopCountV3(b *testing.B) {
+	var tests = makeInputs()
+	for _, v := range tests {
+		PopCountV3(v)
+	}
+}
+
+func BenchmarkPopCountV4(b *testing.B) {
+	var tests = makeInputs()
+	for _, v := range tests {
+		PopCountV4(v)
+	}
+}
